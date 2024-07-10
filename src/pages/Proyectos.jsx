8e26ae09c0b2f1231dd175030b99f2b25709c53c
BenjaminArnello <benjamin.arnello_r@mail.udp.cl>
2024-07-10 17:35:57 -0400

    aa2

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -6,55 +6,12 @@ import {
 } from '@ant-design/icons';
 import React, { useState, useRef, useEffect } from 'react';
 import project1Image from '../images/image.png';
-import { useNavigate } from 'react-router-dom';
+import { useHref, useNavigate } from 'react-router-dom';
 import { crearProyecto, GetProyectosUsuario } from '../services/proyectos';
 import { useDispatch, useSelector } from 'react-redux';
 import {jwtDecode} from 'jwt-decode';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { ImageDB } from '../Config';
-
-const getRandomTags = () => {
-  const tags = ['Arquitectura', 'Infraestructura', 'Educación', 'Tecnología', 'Salud'];
-  const randomTags = [];
-  const numberOfTags = Math.floor(Math.random() * 3) + 1; // Número aleatorio de tags entre 1 y 3
-
-  for (let i = 0; i < numberOfTags; i++) {
-    const randomIndex = Math.floor(Math.random() * tags.length);
-    randomTags.push(`#${tags[randomIndex]}`);
-  }
-
-  return [...new Set(randomTags)]; // Eliminar tags duplicados
-};
-
-const projects = [
-  { id: 1, title: 'Universidad', image: project1Image, tags: getRandomTags() },
-  { id: 2, title: 'Shun Jie', image: project1Image, tags: getRandomTags() },
-  { id: 3, title: 'Shun Jia', image: project1Image, tags: getRandomTags() },
-  { id: 4, title: 'Proyecto Shun', image: project1Image, tags: getRandomTags() },
-  { id: 5, title: 'Proyecto 5', image: project1Image , tags: getRandomTags()},
-  { id: 6, title: 'Proyecto 6', image: project1Image , tags: getRandomTags()},
-  { id: 7, title: 'Proyecto 7', image: project1Image , tags: getRandomTags()},
-  { id: 8, title: 'Proyecto 8', image: project1Image , tags: getRandomTags()},
-  { id: 9, title: 'Proyecto 9', image: project1Image , tags: getRandomTags()},
-  { id: 10, title: 'Proyecto 10', image: project1Image , tags: getRandomTags()},
-  { id: 11, title: 'Proyecto 11', image: project1Image , tags: getRandomTags()},
-  { id: 12, title: 'Proyecto 12', image: project1Image , tags: getRandomTags()},
-  { id: 13, title: 'Proyecto 13', image: project1Image , tags: getRandomTags()},
-  { id: 14, title: 'Proyecto 14', image: project1Image , tags: getRandomTags()},
-  { id: 15, title: 'Proyecto 15', image: project1Image , tags: getRandomTags()},
-  { id: 16, title: 'Proyecto 16', image: project1Image , tags: getRandomTags()},
-  { id: 17, title: 'Proyecto 17', image: project1Image , tags: getRandomTags()},
-  { id: 18, title: 'Proyecto 18', image: project1Image , tags: getRandomTags()},
-  { id: 19, title: 'Proyecto 19', image: project1Image , tags: getRandomTags()},
-  { id: 20, title: 'Proyecto 20', image: project1Image , tags: getRandomTags()},
-  { id: 21, title: 'Proyecto 21', image: project1Image , tags: getRandomTags()},
-  { id: 22, title: 'Proyecto 22', image: project1Image , tags: getRandomTags()},
-  { id: 23, title: 'Proyecto 23', image: project1Image , tags: getRandomTags()},
-  { id: 24, title: 'Proyecto 24', image: project1Image , tags: getRandomTags()},
-  { id: 25, title: 'Proyecto 25', image: project1Image , tags: getRandomTags()},
-  { id: 26, title: 'Proyecto 26', image: project1Image , tags: getRandomTags()},
-  { id: 27, title: 'Proyecto 27', image: project1Image , tags: getRandomTags()},
-];
 
 const PAGE_SIZE = 8; // Number of cards per page
 
@@ -62,6 +19,21 @@ const Proyectos = () => {
   const [transformed, setTransformed] = useState([]);
   const dispatch = useDispatch();
   const proyectos = useSelector(state => state.expensesSlice.proyectosArray); // Corrected state path
+  const [currentPage, setCurrentPage] = useState(1);
+  const [transitioning, setTransitioning] = useState(false);
+  const [position, setPosition] = useState('end');
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible2, setIsModalVisible2] = useState(false);
+  const fileInputRef = useRef(null);
+  const [tags, setTags] = useState([]); // State for tags
+  const [tagInput, setTagInput] = useState(''); // State for tag input
+
+  const [searchTerm, setSearchTerm] = useState(''); // Estado para el término de búsqueda
+  const [file, setFile] = useState(null); // State to store uploaded file
+  const [fileName, setFileName] = useState(''); // State to store uploaded file name
+  const [projectTitle, setProjectTitle] = useState(''); // State to store project title
+  const [tagSearchTerm, setTagSearchTerm] = useState(''); // Estado para la búsqueda por tag
+  const [errorMessage, setErrorMessage] = useState(''); // State to store error message
 
   useEffect(() => {
     const fetchProyectos = async () => {
@@ -87,21 +59,6 @@ const Proyectos = () => {
     console.log("proyectos", proyectos); // This will log the updated proyectos after the state change
   }, [proyectos]);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [transitioning, setTransitioning] = useState(false);
-  const [position, setPosition] = useState('end');
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isModalVisible2, setIsModalVisible2] = useState(false);
-  const [projectName, setProjectName] = useState(''); // State for project name
-  const [tags, setTags] = useState([]); // State for tags
-  const [tagInput, setTagInput] = useState(''); // State for tag input
-  const fileInputRef = useRef(null);
-  const [searchTerm, setSearchTerm] = useState(''); // Estado para el término de búsqueda
-  const [file, setFile] = useState(null); // State to store uploaded file
-  const [fileName, setFileName] = useState(''); // State to store uploaded file name
-  const [projectTitle, setProjectTitle] = useState(''); // State to store project title
-  const [tagSearchTerm, setTagSearchTerm] = useState(''); // Estado para la búsqueda por tag
-  const [errorMessage, setErrorMessage] = useState(''); // State to store error message
 
   useEffect(() => {
     if (transitioning) {
@@ -162,10 +119,18 @@ const Proyectos = () => {
     console.log(proyectos)
   };
 
+
   const postProject = async (imgRef) => {
     const newProject = {
-      titulo: projectTitle, // You can adjust this based on user input
-      foto: imgRef, // You need to implement the logic to get the actual link of the uploaded photo
+      proyecto: {
+        id: 0, // You may need to generate or handle this dynamically
+        titulo: projectTitle, // Adjust based on user input
+        descripcion: "string", // You may need to implement the logic for project description
+        creacion: new Date().toISOString(), // Current timestamp
+        foto: imgRef, // Assuming imgRef is the URL or reference to the uploaded photo
+        activo: true
+      },
+      tagsProyecto: tags // Assuming tags is an array of strings for project tags
     };
 
     try {
@@ -179,37 +144,52 @@ const Proyectos = () => {
     // Simulate POST request (replace with actual API call)
     console.log('Sending POST request with data:', newProject);
 
-    // After sending the request, close the modal
+    setProjectTitle('');
+    setTags([]);
     setIsModalVisible(false);
+    window.location.reload();
+    //window.location.href = '/proyecto#/Proyectos/1/Proyecto';
   };
 
   const handleCreateProject = async () => {
+    if (!projectTitle){
+      setErrorMessage('Por favor, ingrese un titulo para el proyecto.');
+      return;
+    }
+
     if (!file) {
       setErrorMessage('Por favor, sube una imagen antes de crear el proyecto.');
       return;
     }
+
   
     const token = localStorage.getItem('ACCESS_TOKEN');
-    console.log(token);
     const tokenDec = jwtDecode(token);
-    console.log(tokenDec);
     const mail = tokenDec.unique_name;
-    console.log(mail);
-    const imgRef = ref(ImageDB, `files/${mail}-${projectTitle}-${fileName}`);
+    const timestamp = new Date().getTime(); // Add timestamp to ensure uniqueness
+    const fileName1 = `${mail}-${projectTitle}-${timestamp}-${fileName}`; // Assuming projectTitle and fileName are defined somewhere
+  
+    const imgRef = ref(ImageDB, `files/${fileName1}`);
     console.log(imgRef);
   
     try {
+      // Check if the file with the same name exists
+  
       await uploadBytes(imgRef, file);
       console.log('Uploaded a blob or file!');
-      
+  
       const downloadURL = await getDownloadURL(imgRef);
-      console.log('File available at', downloadURL);
-      
+      console.log(downloadURL);
+  
       postProject(downloadURL);
     } catch (error) {
       console.error('Upload failed:', error);
+      setErrorMessage('Hubo un error al subir la imagen, intenta nuevamente');
     }
+
+
   };
+  
 
   const startIndex = (currentPage - 1) * PAGE_SIZE;
 
@@ -218,11 +198,12 @@ const Proyectos = () => {
       project.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
       project.tags.some((tag) => tag.toLowerCase().includes(tagSearchTerm.toLowerCase()))
   );
-
-  const paginatedProjects = filteredProjects.slice(startIndex, startIndex + PAGE_SIZE);
+  
 
   const currentProjects = filteredProjects.slice(startIndex, startIndex + PAGE_SIZE);
 
+
+  
   const renderTags = (tags) => {
     if (!tags || tags.length === 0) {
       return null; // Si no hay tags, no renderizamos nada
@@ -241,86 +222,65 @@ const Proyectos = () => {
 
   const handleTagInputChange = (event) => {
     setTagInput(event.target.value);
+    console.log(tagInput)
   };
 
   const handleAddTag = () => {
     if (tagInput && tags.length < 5) {
       setTags([...tags, tagInput]);
+      
       setTagInput('');
+      
     }
+    console.log(tags)
   };
 
-  const handleProjectNameChange = (event) => {
-    setProjectName(event.target.value);
-  };
-
-  const handleCreateProject = () => {
-    // Aquí puedes manejar la creación del proyecto
-    console.log('Nombre del proyecto:', projectName);
-    console.log('Tags del proyecto:', tags);
-    // Aquí puedes guardar los datos del proyecto, por ejemplo, enviarlos a un servidor
-
-    // Limpiar campos
-    setProjectName('');
-    setTags([]);
-    setIsModalVisible(false);
-    window.location.href = '/proyecto#/Proyectos/1/Proyecto';
-  };
 
   const handleRemoveTag = (tagToRemove) => {
     setTags(tags.filter(tag => tag !== tagToRemove));
   };
 
+
   return (
-    <>
-      <div>
-        <Row gutter={16} justify="space-between">
-          <Col span={8}>
-            <Input
-              placeholder="Buscar por título"
-              value={searchTerm}
-              onChange={handleSearchChange}
-              prefix={<SearchOutlined />}
-            />
-          </Col>
-          <Col span={8}>
-            <Input
-              placeholder="Buscar por tag"
-              value={tagSearchTerm}
-              onChange={(e) => setTagSearchTerm(e.target.value)}
-              prefix={<SearchOutlined />}
-            />
-          </Col>
-          <Col span={8} style={{ textAlign: 'right' }}>
-            <Button type="primary" onClick={showModal}>
-              <FileAddOutlined /> Crear Proyecto
-            </Button>
+    <div>
+      <div style={{ maxWidth: '600px', margin: '0 auto', textAlign: 'center' }}>
+        <Row justify="center" gutter={[16, 16]}>
+          <Col xs={24} sm={12} md={8} lg={6} style={{ textAlign: 'center' }}>
+            <Button
+              type="primary"
+              style={{ width: '80px', height: '40px' }}
+              icon={<SearchOutlined style={{ fontSize: '30px' }} />}
+              iconPosition={position}
+              shape="round"
+              onClick={Buscador}
+            ></Button>
           </Col>
         </Row>
       </div>
-
+      <Row justify="center" style={{ marginTop: '20px' }}>
+        <Col span={24} style={{ textAlign: 'center' }}>
+          <Button
+            shape="round"
+            type="primary"
+            icon={<FileAddOutlined />}
+            iconPosition={position}
+            onClick={showModal}
+            style={{ width: '300px', height: '40px', fontSize: '18px' }}
+          >
+            Crear un Nuevo Proyecto
+          </Button>
+        </Col>
+      </Row>
       <Modal
-        title="Crear Proyecto"
+        style={{ marginTop: '200px' }}
         visible={isModalVisible}
+        footer={null}
         onCancel={handleCancel}
-        footer={[
-          <Button key="cancel" onClick={handleCancel}>
-            Cancelar
-          </Button>,
-          <Button key="create" type="primary" onClick={handleCreateProject}>
-            Crear
-          </Button>,
-        ]}
       >
-        <Input
-          placeholder="Título del proyecto"
-          value={projectTitle}
-          onChange={handleTitleChange}
-        />
-        <Button
-          onClick={handleButtonClick}
+        <div
           style={{
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             height: '100%',
@@ -329,8 +289,8 @@ const Proyectos = () => {
           <Input
             shape="round"
             placeholder="Nombre del Proyecto"
-            value={projectName}
-            onChange={handleProjectNameChange}
+            value={projectTitle}
+            onChange={handleTitleChange}
             style={{
               marginBottom: '10px',
               width: '300px',
@@ -370,6 +330,22 @@ const Proyectos = () => {
               </Button>
             ))}
           </div>
+          {/* Preview the uploaded image */}
+          {file && (
+            <div style={{ marginBottom: '10px' }}>
+              <img
+                src={URL.createObjectURL(file)}
+                alt="Preview"
+                style={{ width: '300px', height: '200px', objectFit: 'cover' }}
+              />
+            </div>
+          )}
+          {/* Error message if no image or title */}
+          {errorMessage && (
+            <div style={{ marginBottom: '10px', color: 'red' }}>
+              {errorMessage}
+            </div>
+          )}
           <Button
             shape="round"
             type="primary"
@@ -404,7 +380,102 @@ const Proyectos = () => {
           />
         </div>
       </Modal>
-    </>
+
+      <Modal
+        style={{ marginTop: '200px' }}
+        visible={isModalVisible2}
+        footer={null}
+        onCancel={handleCancel2}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
+          <Input
+            placeholder="Buscar por nombre del proyecto"
+            style={{ marginBottom: '10px', width: '80%' }}
+            onChange={handleSearchChange}
+          />
+          <Input
+            placeholder="Buscar por categoría del proyecto"
+            style={{ marginBottom: '10px', width: '80%' }}
+            onChange={handleTagSearchChange}
+          />
+          <Button
+            shape="round"
+            type="primary"
+            icon={<SearchOutlined />}
+            iconPosition={position}
+            onClick={handleCancel2}
+            style={{ width: '300px', height: '40px', fontSize: '18px' }}
+          >
+            Buscar
+          </Button>
+        </div>
+      </Modal>
+      <Row
+        justify="center"
+        gutter={[16, 16]}
+        style={{ marginTop: '20px', marginBottom: '20px' }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            opacity: transitioning ? 0 : 1,
+            transition: 'opacity 0.3s ease-in-out',
+            width: '100%',
+            justifyContent: 'center',
+          }}
+        >
+          {currentProjects.map((project) => (
+            <Col xs={24} sm={12} md={8} lg={6} key={project.id}>
+              <Card
+                onClick={() => navigate(`/Proyecto/${project.id}`)}
+                hoverable
+                style={{ marginBottom: '30px' }} // Add margin bottom to increase vertical spacing
+                cover={
+                  <img
+                    alt={project.title}
+                    src={project.image}
+                    style={{
+                      backgroundColor: '#f5f5f5',
+                      height: '200px',
+                      objectFit: 'cover',
+                      width: '100%',
+                    }}
+                  />
+                }
+              >
+                <div style={{ marginTop: '10px' }}>
+                  {renderTags(project.tags)}
+                </div>
+                <Card.Meta
+                  title={
+                    <div style={{ fontSize: '120%', marginTop: '10px' }}>
+                      {project.title}
+                    </div>
+                  }
+                />
+              </Card>
+            </Col>
+          ))}
+        </div>
+      </Row>
+      <Row justify="center" style={{ marginTop: '20px' }}>
+        <Pagination
+          current={currentPage}
+          pageSize={PAGE_SIZE}
+          total={transformed.length}
+          onChange={handlePageChange}
+        />
+      </Row>
+    </div>
   );
 };
 
